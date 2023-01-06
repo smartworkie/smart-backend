049a7338e83be98721918779a3de02ae4f120162
mcsoboye <mcsoboye16@gmail.com>
2023-01-06 03:16:27 +0100

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+const express = require('express');
+const cors = require('cors');
+const path= require('path');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const userRoutes= require('./userRoutes');
+const {notFound, errorHandler} = require('./errorMiddleware');
+
+const connectDB = require('./mongo');
+const app = express();
+dotenv.config();
+connectDB();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+ const _dirname = path.resolve();
+
+app.get('/', (req,res) => {
+        res.send('We Are Here')
+    })
+    app.use('/api/users', userRoutes)
+    
+    app.use(errorHandler)
+
+    const PORT = process.env.PORT1 || 500;
+
+//server producion assets
+    app.use(express.static(path.join(__dirname, '../build')));
+    app.get("*"), (req,res) => res.sendFile((path.join(__dirname, './client/build/index.html')));
+
+app.listen(PORT, console.log(`Server is listening on PORT ${PORT}`)
+);
